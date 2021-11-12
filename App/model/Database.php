5e1abafa918bd6ca46af6e9339b7655a56a583fe
Fabien "BioTheWolff" Zoccola <biothewolff@gmx.fr>
2021-11-12 17:34:50 +0100

    setting up the statement preparation

--- App/model/Database.php
+++ App/model/Database.php
@@ -9,6 +9,9 @@ class Database
      */
     private static $pdo;
 
+    private static $tablename = null;
+    private static $primarykey = null;
+
     private static function init_pdo()
     {
         $keys = ['database.adapter', 'database.host', 'database.dbname', 'database.user', 'database.pass', 'database.port'];
@@ -34,6 +37,46 @@ class Database
     {
         if (is_null(self::$pdo)) self::init_pdo();
         return self::$pdo;
+    }
+
+    /**
+     * Prepares the SQL statement to adapt it to the desired table.
+     * Replaces
+     *  - (required) `:tablename` by the current table name
+     *  - (required) `:primary` by the current primary key
+     *  - (optional) `:updatearr=''` by an associative array of parameters to use in PDOStatement prepare (i.e. `login=:login,user=:user`)
+     *  - (optional) `:insertarr` by a bracketed array of statement parameters (i.e. `(:login,:user)`)
+     *
+     * Warning: Only associative array keys are used. This method is prepared to recieve the `$_POST` or `$_GET` arrays
+     *
+     * @param string $query the prepared string to fill in
+     * @param array $arr the array to give. See above for details.
+     * @return string the prepared SQL statement query
+     */
+    private static function prepare_statement(string $query, array $arr): string
+    {
+        if (is_null(static::$tablename) || is_null(static::$primarykey))
+        {
+            throw new RuntimeException("No redefinition of table name or primary key in class " . static::class);
+        }
+
+        $query = str_replace(':primary', static::$primarykey, str_replace(':tablename', static::$tablename, $query));
+
+        if (strpos($query, ":updatearr=''") !== false)
+        {
+            $s = "";
+            foreach ($arr as $k => $v) $s .= "$k=:$k,";
+            $query = str_replace(":updatearr=''", trim($s, ','), $query);
+        }
+
+        if (strpos($query, ':insertarr') !== false)
+        {
+            $s = "(";
+            foreach ($arr as $k => $v) $s .= ":$k,";
+            $query = str_replace(":updatearr=''", trim($s, ',') . ")", $query);
+        }
+
+        return $query;
     }
 
 }
