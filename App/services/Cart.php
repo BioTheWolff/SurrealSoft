0788e2e4b5c3acb3c47d19a41bf2674b7a284906
Fabien "BioTheWolff" Zoccola <biothewolff@gmx.fr>
2021-11-19 22:57:55 +0100

    cart now working and displays the right values

--- App/services/Cart.php
+++ App/services/Cart.php
@@ -9,7 +9,7 @@ class Cart
     {
         Session::init();
 
-        if (!array_key_exists(self::CART_PREFIX, $_SESSION)) $_SESSION[self::CART_PREFIX] = serialize([]);
+        if (!array_key_exists(self::CART_PREFIX, $_SESSION)) $_SESSION[self::CART_PREFIX] = [];
     }
 
 
@@ -20,22 +20,16 @@ class Cart
 
         self::ensureStarted();
 
-        /** @var array $cart */
-        $cart = unserialize($_SESSION[self::CART_PREFIX]);
-
-        if (array_key_exists($id, $cart))
+        if (array_key_exists($id, $_SESSION[self::CART_PREFIX]))
         {
             // add the quantity
-            $cart[$id] += $quantity;
+            $_SESSION[self::CART_PREFIX][$id] += $quantity;
         }
         else
         {
             // set the quantity
-            $cart[$id] = $quantity;
+            $_SESSION[self::CART_PREFIX][$id] = $quantity;
         }
-
-        // save the modified cart
-        $_SESSION[self::CART_PREFIX] = $cart;
     }
 
     public static function remove(int $id, int $quantity)
@@ -44,17 +38,11 @@ class Cart
 
         self::ensureStarted();
 
-        /** @var array $cart */
-        $cart = unserialize($_SESSION[self::CART_PREFIX]);
-
-        if (array_key_exists($id, $cart))
+        if (array_key_exists($id, $_SESSION[self::CART_PREFIX]))
         {
-            if ($quantity >= $cart[$id]) unset($cart[$id]); // remove more than we have right now
-            else $cart[$id] -= $quantity;
+            if ($quantity >= $_SESSION[self::CART_PREFIX][$id]) unset($_SESSION[self::CART_PREFIX][$id]); // remove more than we have right now
+            else $_SESSION[self::CART_PREFIX][$id] -= $quantity;
         }
-
-        // save the modified cart
-        $_SESSION[self::CART_PREFIX] = $cart;
     }
 
 
@@ -62,7 +50,8 @@ class Cart
     public static function is_empty(): bool
     {
         self::ensureStarted();
-        return empty(unserialize($_SESSION[self::CART_PREFIX]));
+        return !array_key_exists(self::CART_PREFIX, $_SESSION) ||
+            empty($_SESSION[self::CART_PREFIX]);
     }
 
     public static function get_items(): array
@@ -70,7 +59,7 @@ class Cart
         self::ensureStarted();
         if (self::is_empty()) return [];
 
-        return array_keys(unserialize($_SESSION[self::CART_PREFIX]));
+        return array_keys($_SESSION[self::CART_PREFIX]);
     }
 
     /**
@@ -80,10 +69,10 @@ class Cart
     {
         self::ensureStarted();
 
-        /** @var array $cart */
-        $cart = unserialize($_SESSION[self::CART_PREFIX]);
-
-        foreach ($products as $product) $product->setQuantity($cart[$product->getId()]);
+        if (!self::is_empty())
+        {
+            foreach ($products as $product) $product->setQuantity($_SESSION[self::CART_PREFIX][$product->getId()]);
+        }
 
         return $products;
     }
--- App/view/content/cart/display.php
+++ App/view/content/cart/display.php
@@ -2,7 +2,7 @@
 <?php if(!Cart::is_empty()): ?>
 <ul>
     <?php foreach ($rvar_extra_cart_items ?? [] as $p): /** @var CartProduct $p */ ?>
-        <li><?= $p->getQuantity() ?>x <?= e($p->getName()) ?> : <?= $p->getTotalPrice() ?> (<?= $p->getPrice() ?> à l'unité)</li>
+        <li><?= $p->getQuantity() ?>x <?= e($p->getName()) ?> : <?= $p->getTotalPrice() ?>€ (<?= $p->getPrice() ?>€ à l'unité)</li>
     <?php endforeach; ?>
 </ul>
 <?php else: ?>
