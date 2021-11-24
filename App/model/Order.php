777b377c78b082ce3b7a06c521169eeecd8c73fa
Fabien "BioTheWolff" Zoccola <biothewolff@gmx.fr>
2021-11-25 00:20:58 +0100

    adding create functions; not tested yet

--- App/model/Order.php
+++ App/model/Order.php
@@ -63,4 +63,70 @@ class Order extends Database
     {
         return property_exists($this, $parameter) ? $this->$parameter : $default;
     }
+
+
+    // REDEFINITIONS
+
+    /**
+     * Please do NOT use yourself. Use createOrder instead
+     *
+     * @see Order::createOrder()
+     * @param array $values
+     * @return bool
+     */
+    public static function create(array $values): bool
+    {
+        if (!is_array_full(['clientId','date', 'products'], $values)) return false;
+
+        $products = $values['products'];
+        unset($values['products']);
+
+
+        // create order row
+        $o_stmt = self::getPDO()->prepare(self::prepare_statement("INSERT INTO :tablename (:insertkeys) VALUES (:insertvals)", $values));
+        $o_stmt->execute($values);
+        if ($o_stmt->rowCount() == 0) return false;
+
+        // get the order id
+        $id_stmt = self::getPDO()->prepare(self::prepare_statement("SELECT id FROM :tablename WHERE clientId = :cid AND date = :d"));
+        $id_stmt->execute($values);
+        $orderId = $id_stmt->fetch();
+        if ($orderId === false) return false;
+
+
+        // prepare the products' insertion in the association table
+        $query_insertion = "";
+        for ($i = 0; $i < count($products); ++$i) $query_insertion .= "(?,?,?,?),";
+        $query_insertion = trim($query_insertion, ",");
+
+        // prepare the values' insertion by formatting them to fit the format above
+        $insertion_values = [];
+        foreach ($products as $p)
+        {
+            /* @var CartProduct $p */
+            $insertion_values[] = $orderId;
+            $insertion_values[] = $p->getId();
+            $insertion_values[] = $p->getQuantity();
+            $insertion_values[] = $p->getPrice();
+        }
+
+
+        // finally, insert everything
+        $final_stmt = self::getPDO()->prepare("INSERT INTO surrealsoft__orderedProducts VALUES $query_insertion");
+        $final_stmt->execute($insertion_values);
+
+        return $final_stmt->rowCount() > 0;
+    }
+
+    /**
+     * Please use this function to create a new order in the database
+     *
+     * @param int $clientId the client ID
+     * @param CartProduct[] $products the products in the cart
+     * @return bool if the request failed
+     */
+    public static function createOrder(int $clientId, array $products): bool
+    {
+        return self::create(['clientId' => $clientId, 'date' => date("Y-m-d H:i:s"), 'products' => $products]);
+    }
 }
