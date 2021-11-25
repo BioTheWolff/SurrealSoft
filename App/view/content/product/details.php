0ca3c74b5dcb507570de766a11b10869724dbcca
Noé Tarbouriech <noe.tarbouriech@umontpellier.fr>
2021-11-25 10:21:15 +0100

    improved product details page css

--- App/view/content/product/details.php
+++ App/view/content/product/details.php
@@ -2,24 +2,34 @@
 /** @var Product $p */
 $p = $rvar_extra_product ?? null;
 ?>
+<link rel="stylesheet" href="./assets/css/product.css">
 
-<div>
-    <h2><?= $p->getName() ?></h2>
-    <h4><?= $p->getPrice() ?> €</h4>
+<?php if(empty($p)): ?>
+    <div class="no-content">Ce produit n'existe pas.</div>
+<?php else: ?>
 
-    <p><?= $p->getDescription() ?? 'Aucune description pour ce produit.' ?></p>
-    
-    <?php if (!is_null($p->getCover())): ?>
-        <div>
+<div class="position">
+    <a href="?controller=product">Produits</a> > <?= $p->getName() ?>
+</div>
+<div class="prd">
+    <div class="cover">
+        <?php if (!is_null($p->getCover())): ?>
             <img src="data:image/png;base64,<?= $p->getCover() ?>" alt="Image de couverture du produit <?= $p->getName() ?>">
-        </div>
-    <?php endif; ?>
-
-    <div>
-        <a href="<?= loc('cart', 'add', ['product' => $p->getId()]) ?>">Ajouter au panier</a>
-
-        <?php if(Session::is_admin()): ?>
-            <a href="<?= loc('product', 'update', ['product' => $p->getSlug()]) ?>">Éditer le produit</a>
         <?php endif; ?>
     </div>
+
+    <div class="info">
+        <div class="box">
+            <h2><?= $p->getName() ?></h2>
+            <h4 style="color: var(--btn-color);"><?= $p->getPrice() ?> €</h4>
+
+            <p><?= $p->getDescription() ?? 'Aucune description pour ce produit.' ?></p>
+            <a class="btn btn-primary" href="<?= loc('cart', 'add', ['product' => $p->getId()]) ?>">Ajouter au panier</a>
+
+            <?php if(Session::is_admin()): ?>
+                <a class="btn btn-link" href="<?= loc('product', 'update', ['product' => $p->getSlug()]) ?>">Éditer le produit</a>
+            <?php endif; ?>
+        </div>
+    </div>
 </div>
+<?php endif; ?>
