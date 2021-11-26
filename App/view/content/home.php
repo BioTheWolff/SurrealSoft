2dda66b644dfdb70474029cdfabd41bbdad52eb4
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2021-11-26 14:29:18 +0100

    Homepage with banner and icons

--- App/view/content/home.php
+++ App/view/content/home.php
@@ -1,4 +1,17 @@
-
-<h1>
-    ça marche!
-</h1>
+<link rel="stylesheet" href="./assets/css/home.css">
+<img class="banner" src="./assets/img/banner.png">
+<div class="col">
+    <div class="item">
+        <svg height="21" viewBox="0 0 21 21" width="21" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" transform="translate(2 2)"><path d="m12.8571123 1.79063546c-3.70547974-2.40636667-8.66011018-1.35322746-11.06647684 2.35225226-2.40636667 3.70547972-1.35322746 8.66011018 2.35225226 11.06647678 1.40713892.9138067 2.9944136 1.3287299 4.55387082 1.2889715 2.54712886-.0649393 5.02004606-1.3428829 6.51260596-3.6412237 1.5774991-2.4291355 1.6682799-5.39509184.4997393-7.82805117"/><path d="m4.5 7.5 3 3 8-8"/></g></svg>
+        <h3>Une expertise sans précédent !</h3>
+    </div>
+    <div class="item">
+        <svg height="21" viewBox="0 0 21 21" width="21" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" transform="translate(2 5)"><path d="m2.5.5h12c1.1045695 0 2 .8954305 2 2v7c0 1.1045695-.8954305 2-2 2h-12c-1.1045695 0-2-.8954305-2-2v-7c0-1.1045695.8954305-2 2-2z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/><path d="m0 4h17v2h-17z" fill="currentColor"/></g></svg>
+        <h3>Paiement instantané par carte bleue</h3>
+    </div>
+    <div class="item">
+        <svg height="21" viewBox="0 0 21 21" width="21" xmlns="http://www.w3.org/2000/svg"><path d="m8 13.5172414c4.418278 0 8-3.2845583 8-7.0172414 0-3.73268314-3.581722-6.5-8-6.5s-8 3.02593755-8 6.75862069c0 1.45741942.5460328 2.80709561 1.47469581 3.91098161l-.97469581 4.5803977 3.91607376-2.4472652c1.07810761.4571647 2.29544433.7145066 3.58392624.7145066z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" transform="translate(3 3)"/></svg>
+        <h3>Une équipe clientelle à votre écoute</h3>
+    </div>
+</div>
+<h1>Bienvenue !</h1>
