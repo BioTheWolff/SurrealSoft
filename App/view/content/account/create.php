ace5f996ae43afce716c55dacf3416eb38cbeaea
Suzanne Robert <suzanne.robert@etu.umontpellier.fr>
2021-12-02 09:47:28 +0100

    css page connexion

--- App/view/content/account/create.php
+++ App/view/content/account/create.php
@@ -6,25 +6,25 @@
             Nom :
             <input type="text" name="firstname" placeholder="Dupont" value="<?= $rvar_extra_firstname ?? '' ?>" required>
         </label>
-    <label>
-        Prénom :
-        <input type="text" name="lastname" placeholder="Jean" value="<?= $rvar_extra_lastname ?? '' ?>" required>
-    </label>
-    <label>
-        Email :
-        <input type="email" name="email" placeholder="jean.dupont@gmail.com" value="<?= $rvar_extra_email ?? '' ?>" required>
-    </label>
+        <label>
+            Prénom :
+            <input type="text" name="lastname" placeholder="Jean" value="<?= $rvar_extra_lastname ?? '' ?>" required>
+        </label>
+        <label>
+            Email :
+            <input type="email" name="email" placeholder="jean.dupont@gmail.com" value="<?= $rvar_extra_email ?? '' ?>" required>
+        </label>
 
-    <label>
-        Mot de passe :
-        <input id="password" type="password" name="password"
-               title="8 caractères au minimum, au moins un chiffre, une majuscule, une minuscule" required>
-    </label>
+        <label>
+            Mot de passe :
+            <input id="password" type="password" name="password"
+                   title="8 caractères au minimum, au moins un chiffre, une majuscule, une minuscule" required>
+        </label>
 
-    <label>
-        Confirmation du mot de passe :
-        <input id="password_confirm" type="password" name="password_confirm" required>
-    </label>
+        <label>
+            Confirmation du mot de passe :
+            <input id="password_confirm" type="password" name="password_confirm" required>
+        </label>
     </div>
 
     <button type="submit" class="btn btn-link">Créer un compte</button>
--- App/view/content/login.php
+++ App/view/content/login.php
@@ -1,12 +1,14 @@
+<h2>Connexion</h2>
+<form action="<?= loc(null, 'connect_', ['nonce' => $_GET['nonce'] ?? null]) ?>" method="post" class="connection">
+    <div class="div-connexion">
+        <label>E-mail
+            <input type="email" name="email" value="<?= $rvar_extra_email ?? '' ?>" required>
+        </label>
 
-<form action="<?= loc(null, 'connect_', ['nonce' => $_GET['nonce'] ?? null]) ?>" method="post">
-    <label>E-mail
-        <input type="email" name="email" value="<?= $rvar_extra_email ?? '' ?>" required>
-    </label>
+        <label>Mot de passe
+            <input type="password" name="password" required>
+        </label>
+    </div>
 
-    <label>Mot de passe
-        <input type="password" name="password" required>
-    </label>
-
-    <input type="submit" value="Se connecter">
+    <input id="seconnecter" type="submit" class="btn btn-link" value="Se connecter">
 </form>
